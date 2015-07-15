37eb2ef26b5f9646a9216e2d11df0372e8af9ef6
Wade Novak <wade.novak@gmail.com>
2015-07-15 11:07:22 -0500

    Adds sold out functionality to movies index and show

--- app/helpers/showtimes_helper.rb
+++ app/helpers/showtimes_helper.rb
@@ -3,4 +3,8 @@ module ShowtimesHelper
     showtime.start_time.hour < 12 ? "#{showtime.start_time.strftime("%l:%M")}am" :
                                     "#{showtime.start_time.strftime("%l:%M")}pm"
   end
+
+  def sold_out(showtime)
+    showtime.seats_remaining == 0 ? true : false
+  end
 end
