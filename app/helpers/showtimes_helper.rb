d1d4919e5fd1567f70472263f12f2922a34688ba
Wade Novak <wade.novak@gmail.com>
2015-07-19 17:52:55 -0500

    Adds helper method for showtimes without day

--- app/helpers/showtimes_helper.rb
+++ app/helpers/showtimes_helper.rb
@@ -5,6 +5,12 @@ module ShowtimesHelper
     "#{showtime.start_time.strftime("%A")} #{showtime.start_time.strftime("%-l:%M")}pm"
   end
 
+  def format_showtime_without_day(showtime)
+    showtime.start_time.hour < 12 ?
+    "#{showtime.start_time.strftime("%-l:%M")}am" :
+    "#{showtime.start_time.strftime("%-l:%M")}pm"
+  end
+
   def sold_out(showtime)
     showtime.seats_remaining == 0 ? true : false
   end
--- test/helpers/showtime_helper_test.rb
+++ test/helpers/showtime_helper_test.rb
@@ -3,12 +3,20 @@ require 'test_helper'
 class ShowtimeHelperTest < ActionView::TestCase
   include ShowtimesHelper
 
-  test "start time should be converted into strftime" do
+  test "start time should be converted into strftime with day" do
     showtime1 = Showtime.new(start_time: "2015-07-19 11:00:00")
     assert_equal "Sunday 11:00am", format_showtime(showtime1)
 
     showtime2 = Showtime.new(start_time: "2015-07-19 13:00")
     assert_equal "Sunday 1:00pm", format_showtime(showtime2)
+  end
+
+  test "start time should be converted into strftime without day" do
+    showtime1 = Showtime.new(start_time: "2015-07-19 11:00:00")
+    assert_equal "11:00am", format_showtime_without_day(showtime1)
+
+    showtime2 = Showtime.new(start_time: "2015-07-19 13:00")
+    assert_equal "1:00pm", format_showtime_without_day(showtime2)
   end
 
   test "sold out returns true is seats remaining equals zero" do
