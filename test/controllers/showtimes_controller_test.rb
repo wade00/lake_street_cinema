c5d8f22139f11122f2a9c7298016208b26c74ed1
Wade Novak <wade.novak@gmail.com>
2015-07-19 23:06:00 -0500

    Cleans up misc files, CSS and updates Readme

--- test/controllers/showtimes_controller_test.rb
+++ test/controllers/showtimes_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class ShowtimesControllerTest < ActionController::TestCase
-  # test "should get show" do
-  #   get :show, { movie_id: movies(:interstellar).id, id: showtimes(:today_showtime).id }
-  #   assert_response :success
-  #   assert_not_nil assigns(:movie)
-  #   assert_not_nil assigns(:showtime)
-  # end
+  test "should get show" do
+    get :show, { movie_id: movies(:interstellar).id, id: showtimes(:tomorrow_showtime).id }
+    assert_response :success
+    assert_not_nil assigns(:movie)
+    assert_not_nil assigns(:showtime)
+  end
 end
